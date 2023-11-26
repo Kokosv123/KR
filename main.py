288fab4e55e224d9915762d9e768bcef58780e19
Kokosv <memebed441@mail.com>
2023-11-26 09:01:24 +0200

    123

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from colorama import Fore, Style
 import os
 import random
+import time
 
 
 def create_answer_file(answer):
@@ -11,6 +12,7 @@ def create_answer_file(answer):
 
 
 def check_answer(correct_answer, user_answer):
+    time.sleep(1)
     if user_answer == correct_answer:
         print(Fore.GREEN + "Правильный ответ!" + Style.RESET_ALL)
         return True
@@ -21,6 +23,7 @@ def check_answer(correct_answer, user_answer):
 
 def logic_test():
     print("Привет! Это мини тест на твою логику, пройдешь ли ты его?)")
+
     questions = [
         ("==============Что у тебя написанно в txt файле?==============", "1239"),
         ("==============Сколько вышло частей Half Life (не считая эпизодов)?==============", "2"),
@@ -35,6 +38,7 @@ def logic_test():
 
     for question_text, correct_answer in questions:
         user_answer = input(question_text + " ")
+        print("Погодика, дай-ка проверю...")
         if question_text == "==============Что у тебя написанно в txt файле?==============":
             with open(file_path, 'r') as file:
                 if check_answer(file.read().strip(), user_answer):
