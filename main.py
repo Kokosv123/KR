d1256a5deebc59d8d8a2b2dd1f998c7cdf20af78
Kokosv <memebed441@mail.com>
2023-12-02 18:39:41 +0200

    123

--- main.py
+++ main.py
@@ -1,56 +1,81 @@
 from colorama import Fore, Style
-import os
 import random
 import time
 
+# Функция для создания файла с результатами теста
+def create_answer_file(answer, save_results):
+    if save_results:
+        file_path = 'test_results.txt'
+        with open(file_path, 'a') as file:
+            file.write(answer + '\n')
+        return file_path
 
-def create_answer_file(answer):
-    file_path = 'answer.txt'
-    with open(file_path, 'w') as file:
-        file.write(answer)
-    return file_path
-
-
+# Функция для проверки правильности ответа
 def check_answer(correct_answer, user_answer):
-    time.sleep(1)
+    # Сравнение ответа пользователя с правильным ответом
     if user_answer == correct_answer:
+        # Вывод сообщения о правильном ответе
         print(Fore.GREEN + "Правильный ответ!" + Style.RESET_ALL)
         return True
     else:
+        # Вывод сообщения об ошибке
         print(Fore.RED + "Неправильный ответ!" + Style.RESET_ALL)
         return False
 
-
+# Основная функция теста
 def logic_test():
-    print("Привет! Это мини тест на твою логику, пройдешь ли ты его?)")
+    # Создание файла с заранее заданным ответом перед началом теста
+    pre_defined_answer_file_path = 'predefined_answer.txt'
+    with open(pre_defined_answer_file_path, 'w') as file:
+        file.write("1239")
 
+    save_results = input("Хотите сохранить результаты в файл? (да/нет): ").lower() == 'да'
+
+    # Вопросы теста
     questions = [
-        ("==============Что у тебя написанно в txt файле?==============", "1239"),
-        ("==============Сколько вышло частей Half Life (не считая эпизодов)?==============", "2"),
-        ("==============Когда вышел пайтон? (просто напиши год)==============", "1991"),
-        ("==============Когда вышел первый код программирования? (Shortcode)==============", "1949"),
-        ("==============Как называется, самая популярная торговая площадка?==============", "Steam")
+        ("Что у тебя написанно в txt файле?", "1239"),
+        ("Сколько вышло частей Half Life (не считая эпизодов)?", "2"),
+        ("Когда вышел пайтон? (просто напиши год)", "1991"),
+        ("Когда вышел первый код программирования? (Shortcode)", "1949"),
+        ("Как называется, самая популярная торговая площадка?", "Steam"),
+        ("В каком году человек впервые высадился на Луну?", "1969"),
+        ("Какой химический элемент имеет атомный номер 8?", "Кислород")
     ]
 
+    # Перемешивание вопросов
     random.shuffle(questions)
 
-    file_path = create_answer_file("1239")
     correct_answers = 0
+    total_questions = len(questions)
 
     for question_text, correct_answer in questions:
+        start_time = time.time()
         user_answer = input(question_text + " ")
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+
+        if elapsed_time > 10:
+            print(Fore.YELLOW + "Время вышло!" + Style.RESET_ALL)
+            continue
+
+        # Искусственная задержка для имитации проверки ответа
         print(Fore.WHITE + "Погодика, дай-ка проверю...")
-        if question_text == "==============Что у тебя написанно в txt файле?==============":
-            with open(file_path, 'r') as file:
+        time.sleep(0.5)
+
+        # Особая логика для вопроса с чтением файла
+        if question_text == "Что у тебя написанно в txt файле?":
+            with open(pre_defined_answer_file_path, 'r') as file:
                 if check_answer(file.read().strip(), user_answer):
                     correct_answers += 1
-                else:
-                    break
         elif check_answer(correct_answer, user_answer):
             correct_answers += 1
-        else:
-            break
 
-    print(f"Тест окончен! Правильных ответов: {correct_answers} из {len(questions)}.")
+    # Вывод результатов теста
+    result = f"Тест окончен! Правильных ответов: {correct_answers} из {total_questions}."
+    print(result)
 
+    if save_results:
+        create_answer_file(result, save_results)
+
+# Запуск теста
 logic_test()
